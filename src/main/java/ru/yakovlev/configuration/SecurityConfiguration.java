543b8c8598aa8d922b485941fbadbb13c252d567
Alexander Yakovlev <sanyakovlev@yandex.ru>
2021-02-03 14:33:31 +0700

    feat: Argon2 password encoder added

---
+++ src/main/java/ru/yakovlev/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package ru.yakovlev.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Map;
+
+/**
+ * Spring Security configuration
+ *
+ * @author Yakovlev Alexander (sanyakovlev@yandex.ru)
+ * @since 0.3.1
+ */
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder argon2Encoder() {
+        return new DelegatingPasswordEncoder("argon2", Map.of("argon2", new Argon2PasswordEncoder()));
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        return http.build();
+    }
+}
