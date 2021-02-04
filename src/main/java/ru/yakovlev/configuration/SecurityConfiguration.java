7d636177a26c58d2928c06dab55ac5b4b1df0bf1
Alexander Yakovlev <sanyakovlev@yandex.ru>
2021-02-04 10:23:40 +0700

    refactor: Checkstyle violations fixed

--- src/main/java/ru/yakovlev/configuration/SecurityConfiguration.java
+++ src/main/java/ru/yakovlev/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package ru.yakovlev.configuration;
 
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +9,9 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Map;
 
 /**
- * Spring Security configuration
+ * Spring Security configuration.
  *
  * @author Yakovlev Alexander (sanyakovlev@yandex.ru)
  * @since 0.3.1
@@ -21,7 +21,8 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder argon2Encoder() {
-        return new DelegatingPasswordEncoder("argon2", Map.of("argon2", new Argon2PasswordEncoder()));
+        return new DelegatingPasswordEncoder(
+                "argon2", Map.of("argon2", new Argon2PasswordEncoder()));
     }
 
     @Bean
